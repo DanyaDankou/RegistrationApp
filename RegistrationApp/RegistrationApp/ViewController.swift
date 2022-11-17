4c3b4b390954d83ef59d87fbed882951e7fd3598
DanyaDankou <93947618+DanyaDankou@users.noreply.github.com>
2022-11-17 13:06:46 +0300

    Project created

---
+++ RegistrationApp/RegistrationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationApp
+//
+//  Created by comp on 17.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
